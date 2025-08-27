aeb6ddfbacd7b4453c6ac11aeef5e7f4b805eb15
Jing Liu <jintonic@physino.xyz>
2025-08-27 17:08:53 -0500

    enabled SD card logging

--- firmware.c
+++ firmware.c
@@ -1,6 +1,6 @@
 #include <pico/stdlib.h>
 #define n 16 // FIXME: the code only record 7 or 8 waveform samples currently
-uint8_t waveform[n] = {0}, max, i, j;
+uint8_t waveform[n] = {0}, max, i = 0, smpl_ch, ctrl_ch, comparator = 28;
 uint8_t *pointer2wf = &waveform[0];
 
 #include <hardware/adc.h>
@@ -19,27 +19,30 @@ void daq_init() // Waveform digitization using Pi's ADC and DMA
     );
     adc_set_clkdiv(0); // fastest data taking
 
-    dma_channel_config cfg0 = dma_channel_get_default_config(0);
-    channel_config_set_transfer_data_size(&cfg0, DMA_SIZE_8);
-    channel_config_set_read_increment(&cfg0, false);
-    channel_config_set_write_increment(&cfg0, true);
-    channel_config_set_dreq(&cfg0, DREQ_ADC);
-    channel_config_set_chain_to(&cfg0, 1);
-    dma_channel_configure(0, &cfg0,
+    smpl_ch = dma_claim_unused_channel(true); // avoid conflict with SD DMA usage
+    ctrl_ch = dma_claim_unused_channel(true);
+
+    dma_channel_config smpl_cfg = dma_channel_get_default_config(smpl_ch);
+    channel_config_set_transfer_data_size(&smpl_cfg, DMA_SIZE_8);
+    channel_config_set_read_increment(&smpl_cfg, false);
+    channel_config_set_write_increment(&smpl_cfg, true);
+    channel_config_set_dreq(&smpl_cfg, DREQ_ADC);
+    channel_config_set_chain_to(&smpl_cfg, ctrl_ch);
+    dma_channel_configure(smpl_ch, &smpl_cfg,
                           waveform,      // Destination (memory)
                           &adc_hw->fifo, // Source address (ADC FIFO)
                           n,             // Number of transfers
                           false          // Don't start immediately
     );
 
-    dma_channel_config cfg1 = dma_channel_get_default_config(1);
-    channel_config_set_transfer_data_size(&cfg1, DMA_SIZE_32);
-    channel_config_set_read_increment(&cfg1, false);
-    channel_config_set_write_increment(&cfg1, false);
-    channel_config_set_chain_to(&cfg1, 0);
-    dma_channel_configure(1, &cfg1,
-                          &dma_hw->ch[0].write_addr, // Destination
-                          &pointer2wf,               // Source
+    dma_channel_config ctrl_cfg = dma_channel_get_default_config(ctrl_ch);
+    channel_config_set_transfer_data_size(&ctrl_cfg, DMA_SIZE_32);
+    channel_config_set_read_increment(&ctrl_cfg, false);
+    channel_config_set_write_increment(&ctrl_cfg, false);
+    channel_config_set_chain_to(&ctrl_cfg, smpl_ch);
+    dma_channel_configure(ctrl_ch, &ctrl_cfg,
+                          &dma_hw->ch[smpl_ch].write_addr, // Destination
+                          &pointer2wf,                     // Source
                           1, false);
 }
 
@@ -56,8 +59,8 @@ void oled_init()
     ssd1306_init(&oled, 128, 64, 0x3C, i2c0); // must be after previous line
 }
 
-#include <hw_config.h>
-static spi_t spis[] = {{ // SPI connections to SD cards
+#include <hw_config.h> // declaring spi_t, sd_card_t & the following 4 functions
+static spi_t spis[] = {{
     .hw_inst = spi1,
     .miso_gpio = 12, // GPIO number (not pin number)
     .mosi_gpio = 15,
@@ -65,9 +68,9 @@ static spi_t spis[] = {{ // SPI connections to SD cards
     .baud_rate = 12500 * 1000,
 }};
 static sd_card_t sd_cards[] = {{
-    .pcName = "0:",  // Name used to mount device
-    .spi = &spis[0], // Pointer to the SPI driving this card
-    .ss_gpio = 13,   // The SPI slave select GPIO for this SD card
+    .pcName = "0:",  // name used to mount device
+    .spi = &spis[0], // pointer to the SPI driving this card
+    .ss_gpio = 13,   // SPI slave select GPIO for this SD card
     .use_card_detect = false,
 }};
 size_t sd_get_num() { return count_of(sd_cards); }
@@ -87,89 +90,107 @@ spi_t *spi_get_by_num(size_t num)
         return NULL;
 }
 
-FIL file; // Global file object for writing to SD card
-#include <rtc.h>
-#include <f_util.h>
+#include <rtc.h>    // provide time_init()
+#include <stdio.h>  // provide sprintf()
+#include <f_util.h> // provide f_...()
+FIL file;           // file object
+DIR dir;            // directory object
+FILINFO fo;         // file information object
+char filename[12], msg[100];
 void sd_card_init()
 {
-    time_init(); // Initialize time for FatFs
-    FRESULT fr = f_mount(&(sd_cards[0].fatfs), sd_cards[0].pcName, 1);
-    if (fr != FR_OK)
-        panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
-    const char *const filename = "data.txt";
-    fr = f_open(&file, filename, FA_OPEN_APPEND | FA_WRITE);
-    if (fr != FR_OK && fr != FR_EXIST)
-        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    time_init();
+    f_mount(&sd_cards[0].fatfs, sd_cards[0].pcName, 1);
+    f_opendir(&dir, "0:/");
+    while (1)
+    {
+        FRESULT fr = f_readdir(&dir, &fo); // read an item in directory
+        if (fr != FR_OK || fo.fname[0] == 0)
+            break; // error or end of directory
+        if (fo.fname[0] == 'r' & fo.fname[1] == 'u' & fo.fname[2] == 'n')
+            i++; // count files named like run*.txt
+    }
+    f_closedir(&dir);
+    sprintf(filename, "run%03u.txt", i);
+    f_open(&file, filename, FA_CREATE_NEW | FA_WRITE);
 }
 
-#include <stdio.h>
 int main()
 {
     stdio_init_all();
 
-    daq_init();
-    adc_run(true);
-    dma_channel_start(0);
-
-    gpio_init(22); // output of comparator
-    gpio_set_dir(22, GPIO_IN);
-
-    gpio_init(0); // buzzer
+    gpio_init(0); // buzzer using GPIO0
     gpio_set_dir(0, GPIO_OUT);
 
     gpio_init(PICO_DEFAULT_LED_PIN); // LED on Pico board
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    oled_init(&oled);
+    oled_init(&oled);     // OLED display using I2C
     ssd1306_clear(&oled); // its impact is visible only after ssd1306_show()
-    ssd1306_draw_string(&oled, 12, 0, 2, "Have fun!");
-    ssd1306_draw_string(&oled, 6, 16, 2, "Breadboard");
-    ssd1306_draw_string(&oled, 12, 33, 2, "Radiation");
-    ssd1306_draw_string(&oled, 20, 49, 2, "Detector");
+    ssd1306_draw_string(&oled, 0, 0, 2, "open file");
     ssd1306_show(&oled);
 
-    sd_card_init();
-    ssd1306_clear(&oled);
-    if (f_printf(&file, "Hello, world!\n") < 0)
-        ssd1306_draw_string(&oled, 0, 0, 1, "f_printf failed");
+    sd_card_init(); // SD card using DMA and SPI
+    if (f_printf(&file, "# ms, \t height\n") < 0)
+        ssd1306_draw_string(&oled, 0, 24, 2, "failed!");
     else
-        ssd1306_draw_string(&oled, 0, 0, 1, "Data logging started");
+        ssd1306_draw_string(&oled, 0, 24, 2, filename);
     ssd1306_show(&oled);
 
-    char words[16];
-    for (i = 0; i < 5000; i++)
+    daq_init();                 // DAQ using ADC and DMA on Pico board
+    adc_run(true);              // start ADC
+    dma_channel_start(smpl_ch); // start DMA
+
+    gpio_init(comparator); // trigger signal from comparator
+    gpio_set_dir(comparator, GPIO_IN);
+
+    uint64_t ms;        // time in milliseconds since program start
+    uint32_t nevts = 0; // count of recorded events
+    while (true)
     {
-        if (gpio_get(22) == 0)
-            continue;
-        adc_run(false);    // stop ADC temporarily to avoid overwriting waveform
-        gpio_put(0, true); // turn on buzzer
-        gpio_put(PICO_DEFAULT_LED_PIN, true);
+        if (gpio_get(comparator) == 0) // if no signal from comparator
+            continue;                  // check again
 
-        max = 0;
-        for (j = 0; j < n; j++)
-            if (waveform[j] > max)
-                max = waveform[j];
-        printf("%hhu\n", max);
+        adc_run(false);                       // stop to avoid overwriting wf
+        gpio_put(0, true);                    // turn on buzzer
+        gpio_put(PICO_DEFAULT_LED_PIN, true); // turn on LED
 
-        sprintf(words, "%hhu\n", max);
-        ssd1306_clear(&oled);
-        ssd1306_draw_string(&oled, 8, 24, 2, words);
-        ssd1306_show(&oled);
+        max = 0; // max ADC value in this waveform
+        for (i = 0; i < n; i++)
+            if (waveform[i] > max)
+                max = waveform[i];
+        ms = to_ms_since_boot(get_absolute_time());
+        sprintf(msg, "%llu, \t %hhu\n", ms, max);
+        f_printf(&file, msg);
 
-        adc_run(true);                         // restart ADC after waveform analysis
+        if (nevts % 1000 == 0) // update OLED every 100 events
+        {
+            ssd1306_clear(&oled);
+            ssd1306_draw_string(&oled, 0, 0, 2, filename);
+            sprintf(msg, "%u evts", nevts + 1);
+            ssd1306_draw_string(&oled, 0, 24, 2, msg);
+            sprintf(msg, "in %u s", ms / 1000);
+            ssd1306_draw_string(&oled, 0, 49, 2, msg);
+            ssd1306_show(&oled);
+        }
+
+        adc_run(true);                         // restart after wf analysis
         gpio_put(0, false);                    // turn off buzzer
         gpio_put(PICO_DEFAULT_LED_PIN, false); // turn off LED
+        nevts++;
+        if (nevts >= 5000)
+            break;
     }
-
-    FRESULT fr = f_close(&file);
-    if (fr != FR_OK)
-        sprintf(words, "f_close error: %s (%d)\n", FRESULT_str(fr), fr);
-    ssd1306_clear(&oled);
-    ssd1306_draw_string(&oled, 8, 24, 2, words);
-    ssd1306_show(&oled);
+    adc_run(false); // stop ADC
+    f_close(&file);
     f_unmount(sd_cards[0].pcName);
+
     ssd1306_clear(&oled);
-    ssd1306_draw_string(&oled, 8, 24, 2, "DAQ stopped");
+    sprintf(msg, "%d events", nevts);
+    ssd1306_draw_string(&oled, 0, 0, 2, msg);
+    ssd1306_draw_string(&oled, 0, 24, 2, filename);
+    sprintf(msg, "%.2f Hz", (float)nevts / ms * 1000);
+    ssd1306_draw_string(&oled, 0, 49, 2, msg);
     ssd1306_show(&oled);
 
     return 0;
